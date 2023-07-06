61a1da5b088d4b928fdfa3a1baa1b4960df3d427
brendanf <brendan.furneaux@gmail.com>
2023-07-06 15:51:49 +0300

    save workspaces for errors

--- _targets.R
+++ _targets.R
@@ -2,7 +2,7 @@ library(targets)
 library(tarchetypes)
 library(magrittr)
 
-tar_option_set(format = "qs")
+tar_option_set(format = "qs", workspace_on_error = TRUE)
 
 source("scripts/00_util.R")
 source("scripts/01_functions.R")
